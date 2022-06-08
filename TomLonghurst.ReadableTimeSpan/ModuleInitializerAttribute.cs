988c6b8675c462311fa853ed98cce02559978335
Tom Longhurst <thomhurst@gmail.com>
2022-06-08 10:26:23 +0100

    Remove unused attribute

--- TomLonghurst.ReadableTimeSpan/ModuleInitializerAttribute.cs
+++
@@ -1,5 +0,0 @@
-﻿// ReSharper disable once CheckNamespace
-namespace System.Runtime.CompilerServices;
-
-[AttributeUsage(AttributeTargets.Method)]
-public sealed class ModuleInitializerAttribute : Attribute { }
