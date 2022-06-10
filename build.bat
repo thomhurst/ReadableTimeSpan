e1bc8e26b27503c829c6ec9dcd36312682c97d35
Tom Longhurst <thomhurst@gmail.com>
2022-06-10 11:10:11 +0100

    Update bat for new project

--- build.bat
+++ build.bat
@@ -4,4 +4,6 @@ set /p version="Enter Version Number to Build With: "
 @echo on
 dotnet pack ".\TomLonghurst.ReadableTimeSpan\TomLonghurst.ReadableTimeSpan.csproj"  --configuration Release /p:Version=%version%
 
+dotnet pack ".\TomLonghurst.ReadableTimeSpan.Newtonsoft.Json\TomLonghurst.ReadableTimeSpan.Newtonsoft.Json.csproj"  --configuration Release /p:Version=%version%
+
 pause
